bd9adc4f5a8cfd7e30b7f4628f0b15d8a1f4fd6d
Carlos <ratiosu.cct@gmail.com>
2019-11-27 22:42:12 +0100

    Las vocales solo funcionan de esta guisa...

--- src/Utilidades.cpp
+++ src/Utilidades.cpp
@@ -18,10 +18,17 @@ string Utilidades::tolow(string input)
     {
         if (input[i] == (char) 0xC3){
             out.push_back(input[i++]);
-            if ((input[i] >> 5 & 1) == 0)
+            // Tengo que usar un switch, tengo otra solución que imprimiría calçot en lugar de calÇot
+            switch(input[i])
             {
-                input[i] |= 1 << 5;
-                out.push_back(input[i]);
+                case (char) 0x81: out.push_back((char) 0xA1); break;
+                case (char) 0x89: out.push_back((char) 0xA9); break;
+                case (char) 0x8D: out.push_back((char) 0xAD); break;
+                case (char) 0x93: out.push_back((char) 0xB3); break;
+                case (char) 0x9A: out.push_back((char) 0xBA); break;
+                case (char) 0x9C: out.push_back((char) 0xBC); break;
+                case (char) 0x91: out.push_back((char) 0xB1); break;
+                default: out.push_back(input[i]);
             }
         }
         else if (input[i] > 0x40 && input[i] < 0x5B) // MAYUS
